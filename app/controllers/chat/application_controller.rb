b69d64fadcb3356d62e49a748a8d6c0434ecea3b
nomod <m.ryadn@gmail.com>
2018-01-08 14:56:42 +0300

    08.01.18-42

--- app/controllers/chat/application_controller.rb
+++ app/controllers/chat/application_controller.rb
@@ -1,5 +1,5 @@
 module Chat
-  class ApplicationController < ::ApplicationController
+  class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     before_action :check_session
